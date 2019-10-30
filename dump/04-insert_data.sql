dcbf2a9e5494d536da080163a18a84de396df0ef
Leo Nikolaev <nikolaev@kasatkina.org>
2019-10-30 14:55:27 +0300

    removed dump from master

--- dump/04-insert_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users(first_name, last_name, birth_year) VALUES('Sergey', 'Sergeev', 1984);
-INSERT INTO users(first_name, last_name, birth_year) VALUES('Andrey', 'Andreev', 1983);
-INSERT INTO users(first_name, last_name, birth_year) VALUES('Vassile', 'Vasiliev', 1992);
-
-INSERT INTO projects(project_name, project_leader) VALUES('Alpha', 2);
-INSERT INTO projects(project_name, project_leader) VALUES('Beta', 3);
-INSERT INTO projects(project_name, project_leader) VALUES('Gamma', 3);
-INSERT INTO projects(project_name, project_leader) VALUES('Delta', 4);
